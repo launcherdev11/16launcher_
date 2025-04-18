d2bb46b585c1278415eb931e79a9f07abfd7e132
16steyy <pavlovkamil73@gmail.com>
2025-04-18 14:54:47 +0300

    test

--- push.bat
+++ push.bat
@@ -1,7 +1,15 @@
 @echo off
 cd /d %~dp0
+echo.
+echo ====== Git статус перед коммитом ======
+git status
+echo.
+
+set /p msg=Введите комментарий к коммиту: 
 git add .
-set /p msg=Commit message: 
 git commit -m "%msg%"
 git push origin main
+
+echo.
+echo ====== Пуш завершён ======
 pause
